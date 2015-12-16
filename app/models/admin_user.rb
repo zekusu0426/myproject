793a5ee6b82f51989add9da670b40946395778ab
zekusu0426 <breaktime.0426@gmail.com>
2015-12-16 07:27:07 +0000

    design update,and index,login_display add

---
+++ app/models/admin_user.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: admin_users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           default(""), not null
+#  encrypted_password  :string           default(""), not null
+#  remember_created_at :datetime
+#  sign_in_count       :integer          default(0), not null
+#  current_sign_in_at  :datetime
+#  last_sign_in_at     :datetime
+#  current_sign_in_ip  :string
+#  last_sign_in_ip     :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_admin_users_on_email  (email) UNIQUE
+#
+
+class AdminUser < ActiveRecord::Base
+  has_and_belongs_to_many :roles
+  def has_role?(name)
+    self.roles.where(name: name).length > 0
+  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'top/index'
+
+  devise_for :admin_users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :boxes
   resources :sites
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'top#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
