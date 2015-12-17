ad9f80a13874ce870474959b3c15cc0eac060a6d
zekusu0426 <breaktime.0426@gmail.com>
2015-12-17 09:23:02 +0000

    Add Redirect and add_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate, if: :require_auth_controller?
+
+  REQUIRE_AUTH_CONTROLLERS = [BoxesController, SitesController,TopController]
+
+  def require_auth_controller?
+    REQUIRE_AUTH_CONTROLLERS.include?(self.class)
+  end
+
+  def authenticate
+    redirect_to(new_admin_user_session_path) unless admin_user_signed_in?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+AdminUser.delete_all
+AdminUser.create(email: "admin@admin.com",
+                 password: "11111111",
+                 password_confirmation: "11111111")
