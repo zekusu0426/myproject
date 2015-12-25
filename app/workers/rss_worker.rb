376e324fcf65820aee39587233b78ed423257e5e
zekusu0426 <breaktime.0426@gmail.com>
2015-12-25 08:43:30 +0000

    Update rss_worker

--- app/workers/rss_worker.rb
+++ app/workers/rss_worker.rb
@@ -2,8 +2,25 @@ class RssWorker
   include Sidekiq::Worker
   sidekiq_options queue: :rss
 
-  def perform(id, url)
-    @rsslist = Rsslist.find(id)
-    @rsslist.update(url: url)
+  # def perform(id, url)
+  #   @rsslist = Rsslist.find(id)
+  #   @rsslist.update(url: url)
+  # end
+  def perform(id)
+    urls = Rsslist.where(member_id: :id).pluck(:url)
+    urls.each do |url|
+        next unless feed = Feedbag.find(url).first
+        site = Feedjira::Feed.fetch_and_parse(feed)
+        Site.create(:site_name => site.title,
+                    :site_url   => site.url)
+
+        site.entries.each do |entry|
+            Entry.create(:site_url   => site.url,
+                         :entry_name   => entry.title,
+                         :entry_url     => entry.url,
+                         :entry_image   => entry.image,
+                         :entry_summary => entry_summary)
+        end
+    end
   end
 end
